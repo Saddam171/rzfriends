0ac00759d32ae4a82222e006af9ea166accf975f
Saddam171 <92656405+Saddam171@users.noreply.github.com>
2024-09-08 21:03:20 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,84 @@
+module.exports = {
+    server: {
+        /*caso deixe como true, aparecera no topo do site o icone do servidor, nome e um botão escrito entrar, caso não queira apenas deixe como false */
+        ativado: true,
+        id: '1173357091721851001',
+        convite: 'https://discord.gg/ung7'
+    },
+    bot: {
+        token: 'OTA2MzQxNzIyOTA3MzYxMzEw.G_f5hi.SBJN6WwDQib8yN_Q6qJz-nffcGrfIJdV3lmdIo'
+    },
+    pagina: {
+        /*particulas que saem do mouse ao mexer*/
+        particulas: 'https://cdn.discordapp.com/emojis/998607318893723798.png',
+        /* false deixa o avatar quadrado, true deixa o avatar redondo*/
+        avatar_redondo: true,
+        /* recomendo deixar vazio para ficar da cor_fundo, wallpaper fica mt feio */
+        background: '',
+        /*cor do background*/
+        cor_fundo: '#000001',
+        /*titulo que fica na aba do site*/
+        titulo_pagina: 'Resilient rutzen'
+        },
+
+    /*COLOQUE O ID DO USUÁRIO E O MES CORRESPONDENTE AO BOOSTER DELE, CASO ELE NÃO TENHA BOOST COLOQUE null, siga esse exemplo*/
+    usuarios: {
+      '233287596812402689': 'mes_9',/////SADDAM
+      '245823795703709696': 'mes_6',//////HET
+      '127338212426383360': 'mes_24',/////MOHAMED
+      '449805605306368002': 'mes_24', ///// SUZANO
+      "550275967126601730": "mes_3", /// LOLIN CRIA
+      '928662296778125312': 'mes_1',/////M16
+      "1000212139832791070": null, /// dapenha irmao
+      '694775504653254757': "mes_1", ///// IGUINHO
+      '637399204167417856': 'mes_3',/////CLEITIN
+      '816229879241637899': null, /// boquinha 
+      "1101887029626802267": "mes_6", ///IZUNA FILHO
+      '1273455351802630279': 'mes_1',/////FLAVIN RLK
+      '318766610745655297': 'mes_3', ////dn
+      '228319639757651968': 'mes_6',/////VT CRIA
+      '845723242488135712': "mes_1", //// SPECT CRIA
+      '504980333330956313': "mes_15", ///// TZ CRIA
+      '732683965458022490': "mes_1", /// 15
+      '949069047385952256': null, /// RAJADA
+      '990070218258776064': "mes_18", ///cortez
+      '1204435113979674706': 'mes_1', ///// BRUXA IRMÃ
+      '1265479539073745019': null, //////EVELLYN
+      '576148544776437761': null, ///// MEDUSA
+      '729067684855152681': "mes_24", ////// IZA madby
+      '403669817275711488': 'mes_9',//// GB 22MALUCO
+      '98632016404176896': 'mes_6',//// RAFF DEV LORRITA
+      "94490510688792576": "mes_24", /// PH
+      "156114103033790464": "mes_24", ///// BLACK DONO DA MEE66
+      '1175454071512117353': 'mes_2', /// MURALHA
+      '732948922733166643': null, /////BEBEL
+    },
+    /* não mexa */
+    badges: {
+        nitro: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/discordnitro.svg',
+        PremiumEarlySupporter: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/discordearlysupporter.svg',
+        ActiveDeveloper: 'https://raw.githubusercontent.com/efeeozc/discord-badges/6c54af4b871af1e6daef9722f02257af18446129/svg/activedeveloper.svg',
+        HypeSquadOnlineHouse1: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/hypesquadbravery.svg',
+        HypeSquadOnlineHouse2: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/hypesquadbrilliance.svg',
+        HypeSquadOnlineHouse3: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/hypesquadbalance.svg',
+        VerifiedDeveloper: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/discordbotdev.svg',
+        novo_nick: 'https://github.com/mezotv/discord-badges/blob/main/assets/username.png?raw=true',
+        CertifiedModerator: 'https://raw.githubusercontent.com/mezotv/discord-badges/9f80d3bb50b595e23268a1dd2b21ca62b531b353/assets/discordmod.svg',
+        Hypesquad_Events: 'https://raw.githubusercontent.com/mezotv/discord-badges/9f80d3bb50b595e23268a1dd2b21ca62b531b353/assets/hypesquadevents.svg',
+    
+    },
+
+
+    boost: {
+        mes_1: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost1.svg',
+        mes_2: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost2.svg',
+        mes_3: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost3.svg',
+        mes_6: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost4.svg',
+        mes_9: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost5.svg',
+        mes_12: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost6.svg',
+        mes_15: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost7.svg',
+        mes_18: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost8.svg',
+        mes_24: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost9.svg'
+    },
+    
+};
